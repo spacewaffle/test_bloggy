363ec47ae5da89f80f72a1bc69777c0658b85c11
Jon Cheng <jon.syndicate@gmail.com>
2013-08-08 17:31:50 -0400

    basic bloggy setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestBloggy::Application.config.secret_token = '91876a37ddd0e6326fd31fc617ab81b15b01081758c7e791072b56082bc5d3d9ec46808986a4f3dda6c815930bc9ce96ce5c418b502d5ae5af8314e0bbb08d8e'
